4ab832b2a330f7a8636fa761a237d508afabae58
Yurilima20 <yurilima1231@gmail.com>
2024-05-31 15:57:48 -0300

    Inicio projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,29 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoAPi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPi = new ConsumoAPi();
+		var json = consumoAPi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=b4171602");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+
+	}
+}
---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,16 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+@JsonIgnoreProperties(ignoreUnknown = true) // ignora o que você não encontrar
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
+    //@JsonProperty("imdbVotes") String votos esse aqui foi para exemplo,m irei deixar ele aqui para lembrar do property,
+    // ele estava depois do avaliacao
+    // diferencça entre Alias e property, o property ele serializa e desrealiza, enquanto o alias so desarializa, em outras
+    // palavras ele lé title, mas na hora de escrever serializar ele escreve o nome do atributo titulo, como eu indiquei,
+    // o property, le title e na hora de serializar escreve title, no original, o legal é que o JsonAlias('title', 'titulo'),
+    //podemos passar um array como no exemplo de nomes para o alias, para ele procurar, caso formos trabalhar com mais de uma API
+}
